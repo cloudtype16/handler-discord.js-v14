11d590a78d2d7778f354a72baec79b9c28190617
Hugo Melendez <75637430+cloudtype16@users.noreply.github.com>
2022-12-24 18:47:01 +0000

    Chi

--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const { Client, GatewayIntentBits, Partials, Collection } = require("discord.js"
 const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
 const { User, Message, GuildMember, ThreadMember } = Partials;
 
-const client = new Client({ 
-  intents: [Guilds, GuildMembers, GuildMessages], 
-  partials : [User, Message, GuildMember, ThreadMember]
+const client = new Client({
+  intents: [Guilds, GuildMembers, GuildMessages],
+  partials: [User, Message, GuildMember, ThreadMember],
 });
 
 const { loadEvents } = require("./Handlers/eventHandler");
 
-client.config = require("./config.json");
+client.config = require("./config/config.json");
 client.events = new Collection();
+client.commands = new Collection();
 
 loadEvents(client);
 
-client.login(client.config.token)
+client.login(client.config.token);
