d5cb80af856fda54581fd67e899f289ecadde50b
CloudType16 <hugomelendez2006@gmail.com>
2023-03-26 17:28:53 -0600

    chi

--- index.js
+++ index.js
@@ -1,10 +1,8 @@
 const { Client, GatewayIntentBits, Partials, Collection } = require("discord.js");
-const { Guilds, GuildMembers, GuildMessages } = GatewayIntentBits;
-const { User, Message, GuildMember, ThreadMember } = Partials;
 
 const client = new Client({
-  intents: [Guilds, GuildMembers, GuildMessages],
-  partials: [User, Message, GuildMember, ThreadMember],
+  intents: [Object.keys(GatewayIntentBits)],
+  partials: [Object.keys(Partials)],
 });
 
 const { loadEvents } = require("./Handlers/eventHandler");
